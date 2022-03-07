e894e49edcc5c4b744435d0ac5ea41868a012308
clctclub <clctclub@gmail.com>
2022-03-07 13:41:04 +0900

    change ga script

--- pages/_document.js
+++ pages/_document.js
@@ -8,17 +8,6 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}/>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-              `
-            }}
-          />
           <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@100;300;400;500;600;700;800;900&display=swap"
     rel="stylesheet"></link>
           <link href="https://fonts.googleapis.com/css2?family=Syne:wght@100;300;400;500;600;700;800;900&display=swap"
--- pages/index.js
+++ pages/index.js
@@ -125,6 +125,17 @@ function Home({ changeTheme, theme }) {
   return (
     <div className={styles.container}>
       <Head>
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}/>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+            `
+          }}
+        />
         <title>Chainlist.COOL</title>
         {/* <link rel="icon" href="/Chainlist-logo.png" /> */}
       </Head>
